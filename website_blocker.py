107282ca44021853b291697fc88dda1474db15c4
Abraham <abrahamgil2@gmail.com>
2020-03-04 03:50:52 -0600

    Completed the structure of script to detect hours of the day

--- website_blocker.py
+++ website_blocker.py
@@ -1,9 +1,14 @@
 import time
+from datetime import datetime as dt
 
 hosts_path = r"C:\Windows\System32\drivers\etc\hosts"
 redirect="127.0.0.1"
 website_list=["www.facebook.com","facebook.com","www.youtube.com","youtube.com"]
 
 while True:
-    print(1)
+    if dt(dt.now().year,dt.now().month,dt.now().day,8)< dt.now() < dt(dt.now().year,dt.now().month,dt.now().day,16):
+        print("Working hours...")
+    else:
+        print("Freetime hours...")
+
     time.sleep(5)
