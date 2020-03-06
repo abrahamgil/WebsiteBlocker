0b07883174eedf1e0e231cac85d474dd52c72bc7
Abraham <abrahamgil2@gmail.com>
2020-03-06 04:28:19 -0600

    Added the functionality to clear the website blocks during the freetime hours

--- website_blocker.py
+++ website_blocker.py
@@ -8,9 +8,6 @@ website_list=["www.facebook.com","facebook.com","www.youtube.com","youtube.com"]
 
 while True:
     if dt(dt.now().year,dt.now().month,dt.now().day,8)< dt.now() < dt(dt.now().year,dt.now().month,dt.now().day,16):
-        print("Freetime hours...")
-        
-    else:
         print("Working hours...")
         with open(hosts_temp,'r+') as file:
             content=file.read()
@@ -20,4 +17,15 @@ while True:
                     pass
                 else:
                     file.write(redirect+ " " + website + "\n")
+        
+    else:
+        with open(hosts_temp, 'r+') as file:
+            content = file.readlines()
+            file.seek(0)
+            for line in content:
+                if not any(website in line for website in website_list):
+                    file.write(line)
+            file.truncate()
+            print("Freetime hours...")
+        
     time.sleep(5)
